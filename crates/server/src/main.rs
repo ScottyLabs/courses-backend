2093ededbeac45683ba3588c1295bae40825bdad
Anish Pallati <anishp@duck.com>
2025-07-10 17:02:12 -0400

    feat: disable compression

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -7,7 +7,6 @@ use doc::ApiDoc;
 use log::info;
 use routes::{auth, course, root};
 use tower::ServiceBuilder;
-use tower_http::compression::CompressionLayer;
 use tower_oauth2_resource_server::server::OAuth2ResourceServer;
 use utils::shutdown::shutdown_signal;
 use utoipa::OpenApi;
@@ -43,9 +42,7 @@ async fn main() {
         .merge(public_routes)
         .split_for_parts();
 
-    let app = router
-        .merge(SwaggerUi::new("/swagger").url("/openapi.json", ApiDoc::openapi()))
-        .layer(CompressionLayer::new());
+    let app = router.merge(SwaggerUi::new("/swagger").url("/openapi.json", ApiDoc::openapi()));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     info!("Running axum on http://localhost:3000");
